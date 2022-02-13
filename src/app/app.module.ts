3db9d1f74e859f86f63649bcd315dd4c55bd1871
Jochen Kraushaar <jkraush@web.de>
2022-02-13 12:40:09 +0000

    Added PWA dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { HistoryComponent } from './pages/history/history.component';
 import { StartComponent } from './pages/start/start.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -36,6 +38,12 @@ import { AboutComponent } from './pages/about/about.component';
     MatListModule,
     MatTabsModule,
     MatExpansionModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
