99d66b795c8510d1abc9b2632e4c76d66e4a0a91
Jochen Kraushaar <jkraush@web.de>
2022-02-13 13:01:49 +0000

    Added simple update check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe((event) => {
+        if (event.type === 'VERSION_DETECTED') {
+          if (confirm('New version available. Load New Version?')) {
+            this.swUpdate
+              .activateUpdate()
+              .then(() => document.location.reload());
+          }
+        }
+      });
+    }
+  }
 }
