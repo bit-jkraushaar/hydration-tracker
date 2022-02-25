660354730250f34caa82fdb07c4120cd1625f594
Jochen Kraushaar <jkraush@web.de>
2022-02-25 16:27:26 +0000

    First test of removing duplicated code

---
+++ src/notification-utils.js
@@ -0,0 +1,10 @@
+async function scheduleNotification() {
+  // Timestamp is also used in tag, otherwise Chrome does not allow to create
+  // notifications with same tag in notificationclose.
+  const nextNotification = Date.now() + 30 * 1000;
+  return self.registration.showNotification("Hydration Tracker", {
+    tag: "reminder-" + nextNotification,
+    body: "Drink more water",
+    showTrigger: new TimestampTrigger(nextNotification),
+  });
+}
