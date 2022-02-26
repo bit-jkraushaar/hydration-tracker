81527b4544ca0ee7913c31181071adcc42ad9ff7
Jochen Kraushaar <jkraush@web.de>
2022-02-26 14:29:16 +0000

    Send notifications onyl between 8 AM and 8 PM

--- src/notification-utils.js
+++ src/notification-utils.js
@@ -1,7 +1,17 @@
 async function scheduleNotification() {
+  let nextNotification = Date.now() + 60 * 60 * 1000;
+  const lowerBound = new Date().setHours(8, 0, 0);
+  const upperBound = new Date().setHours(20, 0, 0);
+
+  if (nextNotification < lowerBound) {
+    nextNotification = lowerBound;
+  } else if(nextNotification > upperBound) {
+    const lowerBoundDate = new Date(lowerBound);
+    nextNotification = lowerBoundDate.setDate(lowerBoundDate.getDate() + 1);
+  }
+
   // Timestamp is also used in tag, otherwise Chrome does not allow to create
   // notifications with same tag in notificationclose.
-  const nextNotification = Date.now() + 60 * 60 * 1000;
   return self.registration.showNotification("Hydration Tracker", {
     tag: "reminder-" + nextNotification,
     body: "Drink more water",
