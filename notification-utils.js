765cbe74db55d1ac9f86f59bb3a930dd21fa1e92
Jochen Kraushaar <jkraush@web.de>
2022-02-27 13:30:08 +0000

    Auto-generated commit [ci skip]

---
+++ notification-utils.js
@@ -0,0 +1,24 @@
+async function scheduleNotification() {
+  let nextNotification = Date.now() + 60 * 60 * 1000;
+  const lowerBound = new Date().setHours(8, 0, 0);
+  const upperBound = new Date().setHours(20, 0, 0);
+
+  if (nextNotification < lowerBound) {
+    nextNotification = lowerBound;
+  } else if(nextNotification > upperBound) {
+    const lowerBoundDate = new Date(lowerBound);
+    nextNotification = lowerBoundDate.setDate(lowerBoundDate.getDate() + 1);
+  }
+
+  // Timestamp is also used in tag, otherwise Chrome does not allow to create
+  // notifications with same tag in notificationclose.
+  return self.registration.showNotification("Hydration Tracker", {
+    tag: "reminder-" + nextNotification,
+    body: "Don't forget to drink water",
+    silent: false,
+    vibrate: [200, 100, 200],
+    icon: './assets/icons/icon-192x192.png',
+    badge: './assets/icons/icon-96x96.png',
+    showTrigger: new TimestampTrigger(nextNotification),
+  });
+}
---
+++ sw-custom.js
@@ -0,0 +1,40 @@
+importScripts("./notification-utils.js");
+
+(() => {
+  "use strict";
+
+  async function openApp() {
+    // self references the service worker
+    self.clients
+      .matchAll({
+        type: "window",
+      })
+      .then((clientList) => {
+        for (let client of clientList) {
+          if ("focus" in client) {
+            return client.focus();
+          }
+        }
+
+        if (self.clients.openWindow) {
+          return self.clients.openWindow("/hydration-tracker/");
+        }
+      });
+  }
+
+  self.addEventListener("notificationclick", function (event) {
+    event.notification.close();
+
+    event.waitUntil(scheduleNotification().then(() => openApp()));
+  });
+
+  self.addEventListener("notificationclose", (event) => {
+    event.waitUntil(scheduleNotification());
+  });
+
+  self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "ENABLE_NOTIFICATIONS") {
+      event.waitUntil(scheduleNotification());
+    }
+  });
+})();
